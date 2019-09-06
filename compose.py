b8d28095913d32493808d2c1f85d4783d6efb2b1
Gionion <38647969+Gionion@users.noreply.github.com>
2019-09-06 10:34:11 +0200

    Update compose.py

--- compose.py
+++ compose.py
@@ -2,22 +2,70 @@
 
 import requests
 import urllib3.request
+import re
 from itertools import islice
+import argparse
+import sys
 
-def print_compose_version(head):
-    string = ""
+parser = argparse.ArgumentParser()
 
-    for letter in head:
-        string += letter
+parser.add_argument('-p', '--parameter',
+    help='Chosen function, centos-gpg/docker-compose', required=False)
+args = parser.parse_args()
 
-    return string
+if args.parameter == 'compose':
+    def print_compose_version(head):
+        string = ""
 
-response = requests.get('https://docs.docker.com/release-notes/docker-compose/')
+        for letter in head:
+            string += letter
 
-for lines in response:
-    if b"<li><a href=" in lines:
-        if b"class=\"nomunge\"" in lines:
-            head = list(islice(lines.decode(), 27, 33))
-            break
+        return string
 
-print(print_compose_version(head))
+    response = requests.get('https://docs.docker.com/release-notes/docker-compose/')
+    regex = re.compile(r'.*nomunge\"\>(.*)</a.*')
+
+    for lines in response:
+        if b"<li><a href=" in lines:
+            if b"class=\"nomunge\"" in lines:
+                version = lines.decode()
+                break
+
+    m = regex.match(version)
+    v = m.group(1)
+
+    print(print_compose_version(v))
+
+else:
+    def centos_version(head):
+        string = ""
+
+        for letter in head:
+            string += letter
+        
+        return string
+
+    response = requests.get('http://mirror.centos.org/')
+    regex = re.compile(r'\".*centos-(.).*')  
+
+    for lines in response:
+        if b"centos-" in lines:
+            version = lines.decode()
+    
+    m = regex.match(version)
+    v = m.group(1)
+
+    baseurl = "http://mirror.centos.org/centos-{0}/{1}".format(v, v)
+    rpmurl = "{0}/extras/x86_64/Packages/container-selinux-2.68-1.el7.noarch.rpm".format(baseurl)
+    osurl= "{0}/os/x86_64".format(baseurl)
+    gpgurl= "{0}/RPM-GPG-KEY-CentOS-{1}".format(osurl, v)
+
+    if args.parameter == 'rpm':
+        print(centos_version(rpmurl))
+    elif args.parameter == 'os':
+        print(centos_version(osurl))
+    elif args.parameter == 'gpg':
+        print(centos_version(gpgurl))
+    else:
+        sys.exit(0)
+    
